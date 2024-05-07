e7e3b803f806c5d142762e97aadb966b46c17a58
어지민 <eojm9388@gmail.com>
2024-05-07 13:48:30 +0900

    4. component 실습1

---
+++ 4. component/vue_ws_5_1/card-management-project/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <div>
+        <span>
+          명함 관리 페이지
+        </span>
+      </div>
+    </header>
+    <main>
+      <p>명함을 관리하는 페이지입니다. 여기에 명함 목록이 표시됩니다.</p>
+    </main>
+    <article>
+
+    </article>
+    <footer>
+
+    </footer>
+  </div>
+  
+</template>
+
+<style scoped>
+
+header > div {
+  width: 300px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(113, 86, 211);
+  color: white;
+  font-size: 30px;
+  font-weight: 900;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
