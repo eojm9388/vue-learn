246de0bd882566e10e5f61af0e4effac11f9043b
어지민 <eojm9388@gmail.com>
2024-05-07 12:27:18 +0900

    4. component  과제2

---
+++ 4. component/vue_hw_5_4/src/components/ChildPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>자식 페이지입니다.</h3>
+    <p>{{ childInfo.name }}</p>
+    <p>{{ childInfo.age }}</p>
+    <p>{{ childInfo.balance }}</p>
+    <button @click="giveMeAllowance">용돈 주세요.</button>
+  </div>
+</template>
+
+<script setup>
+const child = defineProps({
+  childInfo: Object
+})
+
+const emit = defineEmits(['giveMeAllowance'])
+const giveMeAllowance = function() {
+  emit('giveMeAllowance')
+}
+
+// console.log(child.childInfo)
+
+</script>
+
+<style scoped>
+
+</style>
