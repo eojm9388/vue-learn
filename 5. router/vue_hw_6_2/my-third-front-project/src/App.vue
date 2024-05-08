d20293a23504ccb0064c4ff3799882fcff57a07d
어지민 <eojm9388@gmail.com>
2024-05-08 11:58:02 +0900

    5. router 과제1

---
+++ 5. router/vue_hw_6_2/my-third-front-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import MainPage from './components/MainPage.vue';
+
+</script>
+
+<template>
+  <MainPage />
+  <RouterLink :to="{ name: 'some' }">some</RouterLink> | 
+  <RouterLink :to="{ name: 'other' }">other</RouterLink>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
