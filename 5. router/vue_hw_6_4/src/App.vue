ee99a3872d4f0b9b5f13acdff13bed15cbb7af37
어지민 <eojm9388@gmail.com>
2024-05-08 12:37:52 +0900

    5. router 과제2

---
+++ 5. router/vue_hw_6_4/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import MainPage from './components/MainPage.vue';
+
+</script>
+
+<template>
+  <MainPage />
+  <RouterLink :to="{ name: 'some' }">some</RouterLink> | 
+  <RouterLink :to="{ name: 'other' }">other</RouterLink> | 
+  <RouterLink :to="{ name: 'students' }">students</RouterLink>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ 5. router/vue_hw_6_4/src/views/StudentView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>학생 상세 정보 목록 입니다.</h2>
+    <p v-for="student in students" :key="student.id"
+      @click=goDetail(student)>
+      {{ student.name }} 학생 정보 보기
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const students = ref([
+  { id: 1, name: '김하나'},
+  { id: 2, name: '김두리'},
+  { id: 3, name: '김서이'},
+])
+
+const router = useRouter()
+
+const goDetail = function (student) {
+  
+  router.push({ name: 'student-detail', params:{ 'name': student.name } })
+}
+
+</script>
+
+<style scoped>
+
+</style>
